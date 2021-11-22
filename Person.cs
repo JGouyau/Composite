01056b29843020ffe269b54f486f86d31f6db22f
JGouyau <Gouyau.jimmy@gmail.com>
2021-11-22 11:32:12 +0100

    Ajoutez des fichiers projet.

---
+++ Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Person
+    {
+        List<Person> DescendantDirect { get; set; }
+
+        string Name { get; set; }
+
+        public Person(string name, List<Person> descendant)
+        {
+            Name = name;
+            DescendantDirect = descendant;
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+            DescendantDirect = new List<Person>();
+        }
+
+        public void AjouterDescendant(Person person)
+        {
+            DescendantDirect.Add(person);
+        }
+
+        public override string ToString()
+        {
+            string descendantString;
+            if (DescendantDirect.Count > 0)
+            {
+                descendantString = $"Les descendants direct de {Name} sont : " + String.Join(", ", DescendantDirect.Select(p => p.Name));
+
+                foreach (Person p in DescendantDirect)
+                {
+                    descendantString += "\n" + p.ToString();
+                }
+            }
+            else
+            {
+                descendantString = $"{Name} n'a pas de descendant direct";
+            }
+            return descendantString;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person PetitEnfant1 = new Person("Louis");
+            Person PetitEnfant2 = new Person("Timoté");
+            Person PetitEnfant3 = new Person("Sebastine");
+            Person Enfant1 = new Person("Loic");
+            Person Enfant2 = new Person("Léa");
+            Person Enfant3 = new Person("Cédric");
+            Person Parent1 = new Person("Papa");
+            Person Ancetre = new Person("Le vieux", new List<Person> { Parent1 });
+            Parent1.AjouterDescendant(Enfant1);
+            Parent1.AjouterDescendant(Enfant2);
+            Parent1.AjouterDescendant(Enfant3);
+
+            Enfant2.AjouterDescendant(PetitEnfant1);
+            Enfant2.AjouterDescendant(PetitEnfant2);
+            Enfant3.AjouterDescendant(PetitEnfant3);
+
+            Console.WriteLine(PetitEnfant2.ToString());
+            Console.WriteLine(Enfant2.ToString());
+
+            Console.WriteLine("\n" + Ancetre.ToString());
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
